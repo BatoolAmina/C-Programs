5afcf40e5d6789164fc47d673950649d1d29ca60
Batool Amina <85053802+BatoolAmina@users.noreply.github.com>
2025-04-28 10:05:06 +0530

    Create Prg 8.c

---
+++ Prg 8.c
@@ -0,0 +1,18 @@
+// Write a Program to print number is even or odd
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is odd.\n", num);
+    }
+
+    return 0;
+}
